7d46ff59c3c5c29f3f41a1f93befa991d91f923c
Partynumbers42 <118724611+Partynumbers42@users.noreply.github.com>
2024-11-05 16:51:08 +0100

    init

---
+++ src/phasr/__init__.py
@@ -0,0 +1,10 @@
+"""Package to calculate scattering phase shifts for arbitrary radial potentials using the phase shift method as well as resulting crosssections as mainly used in the context of elastic electron nucleus scattering."""
+
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("phasr")
+except PackageNotFoundError:
+    __version__ = "uninstalled"
+__author__ = "Frederic Noel"
+__email__ = "noel@ipt.unibe.ch"
