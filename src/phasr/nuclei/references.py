0be67bba6a00425348a26e21e0fe185b95b9fceb
Partynumbers42 <118724611+Partynumbers42@users.noreply.github.com>
2025-02-13 15:22:18 +0100

    Correct reference data

--- src/phasr/nuclei/references.py
+++ src/phasr/nuclei/references.py
@@ -1,6 +1,6 @@
 from . import nucleus
 
-import os#, glob
+import os #, glob
 import numpy as np
 
 reference_data_path = os.path.join(os.path.dirname(__file__), 'data/params_FB.txt')
@@ -9,7 +9,7 @@ ai_str_list = ['a'+str(i) for i in range(1,17+1,1)]
 with open( reference_data_path, "rb" ) as file:
     reference_file = np.genfromtxt( file,comments=None,skip_header=2,delimiter=None,dtype=['U5',int,int]+17*[float]+[float],autostrip=True,names=['name', 'A', 'Z']+ai_str_list+['R'])
 
-AZ_onfile = reference_file[['A','Z']]
+onfile = reference_file[['name','A','Z']]
 
 def load_reference_nucleus(Z,A):
     
@@ -19,15 +19,16 @@ def load_reference_nucleus(Z,A):
         raise KeyError('No parameterisation on file for this isotope')
 
     nuclei_ref = []
-
+    
+    counter=1
     for reference in references:
         
         name = reference['name'][0]
         radius = reference['R'][0]
         ai = np.trim_zeros(np.array(list(reference[ai_str_list][0])),trim='b')
-        
-        nucleus_ref = nucleus(name+'_ref',Z,A,ai=ai,R=radius)
+        nucleus_ref=nucleus(name+'_ref'+str(counter),Z,A,ai=ai,R=radius)
         nuclei_ref.append(nucleus_ref)
+        counter+=1
 
     if len(nuclei_ref)==1:
         nuclei_ref = nuclei_ref[0]
--- src/phasr/nuclei/selector.py
+++ src/phasr/nuclei/selector.py
@@ -6,7 +6,6 @@ from .parameterizations.basic import nucleus_gauss, nucleus_uniform
 from .parameterizations.numerical import nucleus_num
 from .parameterizations.coulomb import nucleus_coulomb
 
-
 def nucleus(name,Z,A,**args):
     args = {"name":name,"Z":Z,"A":A,**args}
     if ('ai' in args) and ('R' in args):
@@ -23,50 +22,3 @@ def nucleus(name,Z,A,**args):
         return nucleus_num(**args)
     else:
         return nucleus_coulomb(**args)
-
-import os, glob
-import numpy as np
-
-def load_reference_nucleus(Z,A,nucleus_key):
-    
-    reference_data_path = os.path.join(os.path.dirname(__file__), 'data/')
-    #print(reference_data_path+"*"+nucleus_key+".txt")
-    paths_reference = glob.glob(reference_data_path+"*"+nucleus_key+".txt")
-    #print(paths_reference)
-    #add loading your own results
-    #paths_own = glob.glob(reference_data_path+"*.txt")
-
-    ai_str_list = ['a'+str(i) for i in range(1,17+1,1)]
-
-    references=None
-
-    for path in paths_reference:
-        
-        with open( path, "rb" ) as file:
-            reference_file = np.genfromtxt( file,comments=None,skip_header=2,delimiter=None,dtype=['U5',int,int]+17*[float]+[float],autostrip=True,names=['name', 'A', 'Z']+ai_str_list+['R'])
-    	
-        references=reference_file[np.argwhere(np.logical_and(reference_file['Z']==Z, reference_file['A']==A))]
-
-    if references is None:
-        raise KeyError('No parameterisation on file for this isotope')
-
-    nuclei_ref = []
-
-    for reference in references:
-        
-        if nucleus_key == 'FB':
-            name = reference['name'][0]
-            radius = reference['R'][0]
-            ai = np.trim_zeros(np.array(list(reference[ai_str_list][0])),trim='b')
-            
-            nucleus_ref = nucleus(name+'_ref',Z,A,ai=ai,R=radius)
-            nuclei_ref.append(nucleus_ref)
-   
-    if len(nuclei_ref)==1:
-        nuclei_ref = nuclei_ref[0]
-    
-    return nuclei_ref
-
-# TODO
-def save_reference_nucleus(Z,A,nucleus_type):
-    pass
