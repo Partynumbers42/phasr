06987b6d38a86455d80f641cdfa549e3e15bedf7
Partynumbers42 <118724611+Partynumbers42@users.noreply.github.com>
2025-02-27 15:57:04 +0100

    Experiment Multiprocessing

--- src/phasr/dirac_solvers/post_processing/crosssection.py
+++ src/phasr/dirac_solvers/post_processing/crosssection.py
@@ -14,6 +14,12 @@ import itertools
 
 import time, copy
 
+# multiprocessing
+from multiprocessing import Pool, cpu_count
+# module as master
+from ...utility.mpsentinel import MPSentinel
+MPSentinel.As_master()
+
 parameter_steps={
     'method' : np.array(['DOP853']), #,'LSODA' 
     'N_partial_waves' : np.append(np.arange(100,50-10,-10),np.arange(50,20-5,-5)),
@@ -23,9 +29,154 @@ parameter_steps={
     'phase_difference_limit' : np.append([0],10**np.arange(-15,-6+1,1,dtype=float)),
 }
 
-def optimise_crosssection_precision(energy,theta,nucleus,lepton_mass=0,recoil=True,subtractions=3,crosssection_precision=1e-3,jump_forward_dist=1,verbose=False):
+def crosssection_and_time(energy,theta,nucleus,**args):
+        start_time=time.time()
+        crosssection = crosssection_lepton_nucleus_scattering(energy,theta,nucleus,**args)
+        end_time=time.time()
+        runtime=end_time-start_time
+        return crosssection, runtime
+    
 
-    #parallise?
+def crosssection_lepton_nucleus_scattering_multithreaded_args(energy,theta,nucleus,lepton_mass=0,recoil=True,subtractions=3,verbose=False,args_dict=parameter_steps,N_max_cpu=cpu_count()-1):
+
+    N_params_iter=len(list(itertools.product(*args_dict.values())))
+    N_pools = int(np.min([N_max_cpu,N_params_iter]))
+    
+    results_dict={}
+    crossection_dict={}
+    time_dict={}
+    
+    if MPSentinel.Is_master():
+        params_iter = itertools.product(*args_dict.values())
+        pool_counter=0
+        parameter_counter=0
+        for method,N_partial_waves,atol,rtol,energy_norm,phase_difference_limit in params_iter: 
+            
+            args={'method':method,'N_partial_waves':N_partial_waves,'atol':atol,'rtol':rtol,'energy_norm':energy_norm,'phase_difference_limit':phase_difference_limit,'lepton_mass':lepton_mass,'recoil':recoil,'subtractions':subtractions,'verbose':verbose} 
+            
+            if pool_counter==0:
+                pool = Pool(processes=N_pools)
+            
+            results_dict[parameter_counter] = pool.apply_async(crosssection_and_time, args=(energy,theta,nucleus), kwds=args)
+            print('Queued: ',parameter_counter+1,'/',N_params_iter,' , ',pool_counter+1,'/',N_pools)
+            
+            pool_counter+=1
+            parameter_counter+=1
+            
+            if pool_counter==N_pools:
+                pool_counter=0
+                pool.close()
+                pool.join()
+                
+        for key in results_dict:
+            crosssection_key, runtime_key =  results_dict[key].get()
+            crossection_dict[key] = crosssection_key
+            time_dict[key] = runtime_key
+
+    return crossection_dict, time_dict
+
+
+def optimise_crosssection_precision_multithreaded_args(energy,theta,nucleus,lepton_mass=0,recoil=True,subtractions=3,crosssection_precision=1e-3,jump_forward_dist=1,verbose=False,N_max_cpu=cpu_count()-1):
+    
+    N_params_iter=len(list(itertools.product(*parameter_steps.values())))
+    N_pools = int(np.min([N_max_cpu,N_params_iter]))
+    
+    results_dict={}
+    crosssection_dict={}
+    runtime_dict={}
+    args_dict={}
+    gotten_keys=[]
+    
+    if MPSentinel.Is_master():
+        
+        insufficient_args=[]
+        
+        pool_counter=0
+        parameter_counter=0
+        
+        first=True
+        for method,N_partial_waves,atol,rtol,energy_norm,phase_difference_limit in itertools.product(*parameter_steps.values()): 
+            
+            skip=False
+            
+            args={'method':method,'N_partial_waves':N_partial_waves,'atol':atol,'rtol':rtol,'energy_norm':energy_norm,'phase_difference_limit':phase_difference_limit} 
+            
+            for key in args:
+                index_key = np.where(parameter_steps[key]==args[key])[0][0]
+                if index_key>0:
+                    args_check = copy.copy(args)
+                    args_check[key] = parameter_steps[key][index_key-1]
+                    # skip if more precise calculation was already unsuccessful
+                    if args_check in insufficient_args:
+                        skip=True
+                        insufficient_args.append(args)
+                        #print('skipped')
+                        break
+                
+                if index_key<len(parameter_steps[key]) and not first:
+                    index_best_key = np.where(parameter_steps[key]==best_args[key])[0][0]
+                    # skip until close to the current best again
+                    if index_key < index_best_key-jump_forward_dist:
+                        skip=True
+                        #print('jumped forward')
+                        break
+            
+            if not skip:
+                
+                if pool_counter==0:
+                    pool = Pool(processes=N_pools)
+            
+                args_dict[parameter_counter]=args
+                
+                results_dict[parameter_counter] = pool.apply_async(crosssection_and_time, args=(energy,theta,nucleus), kwds=args)
+                #print('Queued: ',parameter_counter+1,'/',N_params_iter,' , ',pool_counter+1,'/',N_pools)
+
+                pool_counter+=1
+                parameter_counter+=1
+                
+                if pool_counter==N_pools:
+                    print('Queued: ',parameter_counter+1,'/',N_params_iter)
+                    pool.close()
+                    pool.join()
+                    pool_counter=0
+                    
+                    for key in results_dict:
+                        if not (key in gotten_keys):
+                            crosssection_key, runtime_key =  results_dict[key].get()
+                            crosssection_dict[key] = crosssection_key
+                            runtime_dict[key] = runtime_key
+                
+                    if first:
+                        crosssection0=crosssection_dict[0]
+                        best_time=runtime_dict[0]
+                        best_args=args_dict[0]
+                        first=False
+                    
+                    for key in results_dict:
+                        if not (key in gotten_keys):
+
+                            crossections_difference=(crosssection_dict[key]-crosssection0)/crosssection0
+                        
+                            if np.all(crossections_difference<crosssection_precision) and (runtime_dict[key]-best_time)/best_time<1e-2:
+                                best_time=runtime_dict[key]
+                                best_args=args_dict[key]
+                                if True:#verbose:
+                                    print('new best:',best_args)
+                                    print('time:',best_time,'diff:',np.max(crossections_difference))
+                            
+                            if np.any(crossections_difference>crosssection_precision):
+                                insufficient_args.append(args)
+                                
+                            gotten_keys.append(key)
+            else:
+                N_params_iter-=1
+
+    if verbose:
+        print('best time:',best_time)    
+    
+    return best_args
+
+def optimise_crosssection_precision(energy,theta,nucleus,lepton_mass=0,recoil=True,subtractions=3,crosssection_precision=1e-3,jump_forward_dist=1,verbose=False):
 
     insufficient_args=[]
     
@@ -41,11 +192,11 @@ def optimise_crosssection_precision(energy,theta,nucleus,lepton_mass=0,recoil=Tr
             if index_key>0:
                 args_check = copy.copy(args)
                 args_check[key] = parameter_steps[key][index_key-1]
-                # skip if more precise calculation was already unsuccesfull
+                # skip if more precise calculation was already unsuccessful
                 if args_check in insufficient_args:
                     skip=True
                     insufficient_args.append(args)
-                    #print('skiped')
+                    #print('skipped')
                     break
             
             if index_key<len(parameter_steps[key]) and not first:
