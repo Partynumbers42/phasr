83b6f68198de41ab44ff8d7beeaa53910f9121ca
Partynumbers42 <118724611+Partynumbers42@users.noreply.github.com>
2025-02-28 13:00:08 +0100

    Multiprocess for Crosssection

--- src/phasr/dirac_solvers/post_processing/crosssection.py
+++ src/phasr/dirac_solvers/post_processing/crosssection.py
@@ -22,20 +22,19 @@ MPSentinel.As_master()
 
 parameter_steps={
     'method' : np.array(['DOP853']), #,'LSODA' 
-    'N_partial_waves' : np.append(np.arange(100,50-10,-10),np.arange(50,20-5,-5)),
+    'N_partial_waves' : np.append(np.arange(150,50-10,-10),np.arange(50,20-5,-5)),
     'atol' : 10**np.arange(-13,-6+1,1,dtype=float),
     'rtol' : 10**np.arange(-13,-6+1,1,dtype=float),
     'energy_norm': constants.hc*10**np.arange(0,-6-1,-1,dtype=float),
     'phase_difference_limit' : np.append([0],10**np.arange(-15,-6+1,1,dtype=float)),
 }
 
-def crosssection_and_time(energy,theta,nucleus,**args):
+def crosssection_and_time(energy,theta,nucleus,N_processes=1,**args):
         start_time=time.time()
-        crosssection = crosssection_lepton_nucleus_scattering(energy,theta,nucleus,**args)
+        crosssection = crosssection_lepton_nucleus_scattering(energy,theta,nucleus,N_processes=N_processes,**args)
         end_time=time.time()
         runtime=end_time-start_time
         return crosssection, runtime
-    
 
 def crosssection_lepton_nucleus_scattering_multithreaded_args(energy,theta,nucleus,lepton_mass=0,recoil=True,subtractions=3,verbose=False,args_dict=parameter_steps,N_max_cpu=cpu_count()-1):
 
@@ -75,7 +74,6 @@ def crosssection_lepton_nucleus_scattering_multithreaded_args(energy,theta,nucle
 
     return crossection_dict, time_dict
 
-
 def optimise_crosssection_precision_multithreaded_args(energy,theta,nucleus,lepton_mass=0,recoil=True,subtractions=3,crosssection_precision=1e-3,jump_forward_dist=1,verbose=False,N_max_cpu=cpu_count()-1):
     
     N_params_iter=len(list(itertools.product(*parameter_steps.values())))
@@ -106,7 +104,7 @@ def optimise_crosssection_precision_multithreaded_args(energy,theta,nucleus,lept
                 if index_key>0:
                     args_check = copy.copy(args)
                     args_check[key] = parameter_steps[key][index_key-1]
-                    # skip if more precise calculation was already unsuccessful
+                    # skip if more precise calculation was already unsuccessful 
                     if args_check in insufficient_args:
                         skip=True
                         insufficient_args.append(args)
@@ -115,7 +113,7 @@ def optimise_crosssection_precision_multithreaded_args(energy,theta,nucleus,lept
                 
                 if index_key<len(parameter_steps[key]) and not first:
                     index_best_key = np.where(parameter_steps[key]==best_args[key])[0][0]
-                    # skip until close to the current best again
+                    # skip until close to the current best again 
                     if index_key < index_best_key-jump_forward_dist:
                         skip=True
                         #print('jumped forward')
@@ -129,13 +127,13 @@ def optimise_crosssection_precision_multithreaded_args(energy,theta,nucleus,lept
                 args_dict[parameter_counter]=args
                 
                 results_dict[parameter_counter] = pool.apply_async(crosssection_and_time, args=(energy,theta,nucleus), kwds=args)
-                #print('Queued: ',parameter_counter+1,'/',N_params_iter,' , ',pool_counter+1,'/',N_pools)
+                print('Queued: ',parameter_counter+1,'/',N_params_iter,' , ',pool_counter+1,'/',N_pools) 
 
                 pool_counter+=1
                 parameter_counter+=1
                 
                 if pool_counter==N_pools:
-                    print('Queued: ',parameter_counter+1,'/',N_params_iter)
+                    print('Queued: ',parameter_counter,'/',N_params_iter)
                     pool.close()
                     pool.join()
                     pool_counter=0
@@ -155,12 +153,12 @@ def optimise_crosssection_precision_multithreaded_args(energy,theta,nucleus,lept
                     for key in results_dict:
                         if not (key in gotten_keys):
 
-                            crossections_difference=(crosssection_dict[key]-crosssection0)/crosssection0
+                            crossections_difference=np.abs(crosssection_dict[key]-crosssection0)/crosssection0
                         
                             if np.all(crossections_difference<crosssection_precision) and (runtime_dict[key]-best_time)/best_time<1e-2:
                                 best_time=runtime_dict[key]
                                 best_args=args_dict[key]
-                                if True:#verbose:
+                                if True:#verbose: 
                                     print('new best:',best_args)
                                     print('time:',best_time,'diff:',np.max(crossections_difference))
                             
@@ -176,7 +174,7 @@ def optimise_crosssection_precision_multithreaded_args(energy,theta,nucleus,lept
     
     return best_args
 
-def optimise_crosssection_precision(energy,theta,nucleus,lepton_mass=0,recoil=True,subtractions=3,crosssection_precision=1e-3,jump_forward_dist=1,verbose=False):
+def optimise_crosssection_precision(energy,theta,nucleus,lepton_mass=0,recoil=True,subtractions=3,crosssection_precision=1e-3,jump_forward_dist=1,N_processes=1,verbose=False):
 
     insufficient_args=[]
     
@@ -209,10 +207,7 @@ def optimise_crosssection_precision(energy,theta,nucleus,lepton_mass=0,recoil=Tr
 
         if not skip:
 
-            start_time=time.time()
-            crosssection = crosssection_lepton_nucleus_scattering(energy,theta,nucleus,lepton_mass=lepton_mass,recoil=recoil,subtractions=subtractions,verbose=verbose,**args)
-            end_time=time.time()
-            runtime=end_time-start_time
+            crosssection, runtime = crosssection_and_time(energy,theta,nucleus,N_processes=N_processes,lepton_mass=lepton_mass,recoil=recoil,subtractions=subtractions,verbose=verbose,**args)
             
             if first:
                 crosssection0=crosssection
@@ -220,7 +215,7 @@ def optimise_crosssection_precision(energy,theta,nucleus,lepton_mass=0,recoil=Tr
                 best_args=copy.copy(args)
                 first=False
             
-            crossections_difference=(crosssection-crosssection0)/crosssection0
+            crossections_difference=np.abs(crosssection-crosssection0)/crosssection0
         
             if np.all(crossections_difference<crosssection_precision) and (runtime-best_time)/best_time<1e-2:
                 if True:#verbose:
@@ -243,7 +238,120 @@ def recoil_quantities(energy_lab,theta_lab,mass):
     scalefactor_crosssection_CMS = 1+(2*energy_lab/mass)*np.cos(theta_lab)
     return energy_CMS, theta_CMS, scalefactor_crosssection_CMS
 
-def crosssection_lepton_nucleus_scattering(energy,theta,nucleus,lepton_mass=0,recoil=True,subtractions=3,N_partial_waves=100,verbose=False,phase_difference_limit=0,**args):
+def phase_shift_from_partial_wave(nucleus,kappa,energy,lepton_mass,**args):
+    partial_wave_kappa = continuumstates(nucleus,kappa,energy,lepton_mass,**args)
+    partial_wave_kappa.extract_phase_shift()
+    return partial_wave_kappa.phase_shift, partial_wave_kappa.phase_difference
+
+def crosssection_lepton_nucleus_scattering(energy,theta,nucleus,lepton_mass=0,recoil=True,subtractions=3,N_partial_waves=100,verbose=False,phase_difference_limit=0,N_processes=1,**args):
+    if N_processes>1:
+        return crosssection_lepton_nucleus_scattering_multithreaded(energy,theta,nucleus,lepton_mass=lepton_mass,recoil=recoil,subtractions=subtractions,N_partial_waves=N_partial_waves,verbose=verbose,phase_difference_limit=phase_difference_limit,N_max_cpu=N_processes,**args)
+    else:
+        return crosssection_lepton_nucleus_scattering_singlethreaded(energy,theta,nucleus,lepton_mass=lepton_mass,recoil=recoil,subtractions=subtractions,N_partial_waves=N_partial_waves,verbose=verbose,phase_difference_limit=phase_difference_limit,**args)
+
+def crosssection_lepton_nucleus_scattering_multithreaded(energy,theta,nucleus,lepton_mass=0,recoil=True,subtractions=3,N_partial_waves=100,verbose=False,phase_difference_limit=0,N_max_cpu=cpu_count()-1,**args):
+    
+    args['verbose']=verbose
+
+    N_pools = int(np.min([N_max_cpu,N_partial_waves]))
+
+    nucleus_mass=nucleus.mass
+    charge = nucleus.total_charge
+
+    if recoil:
+        energy, theta, scale_crosssection = recoil_quantities(energy,theta,nucleus_mass)
+        if verbose:
+            print('E=',energy,'MeV')
+    else:
+        scale_crosssection = 1
+    
+    phase_shifts = {}
+    phase_differences = {}
+    phase_difference_gr0 = True
+    
+    if MPSentinel.Is_master():
+    
+        # calculate beginning and critical radius only once, since independent on kappa
+        if (not ('beginning_radius' in args)) or (not ('critical_radius' in args)):
+            initialiser = continuumstates(nucleus,-1,energy,lepton_mass,**args)
+        if not 'beginning_radius' in args:
+            args['beginning_radius']=initialiser.solver_setting.beginning_radius
+        if not 'critical_radius' in args:
+            args['critical_radius']=initialiser.solver_setting.critical_radius
+
+        pool_counter=0
+        kappa_counter=0
+        kappa=-1
+    
+        while np.abs(kappa) <= N_partial_waves+1:
+        #for kappa in np.arange(-1,-(N_partial_waves+1+1),-1,dtype=int):
+            
+            if pool_counter==0:
+                pool = Pool(processes=N_pools)
+                results_dict={}
+            
+            if phase_difference_gr0:
+                
+                results_dict[kappa] = pool.apply_async(phase_shift_from_partial_wave, args=(nucleus,kappa,energy,lepton_mass), kwds=args)
+                #print('Queued: kappa=',kappa,' , (',pool_counter+1,'/',N_pools,')')
+                pool_counter+=1
+                #phase_shifts[kappa], phase_differences[kappa] = phase_shift_from_partial_wave(nucleus,kappa,energy,lepton_mass,**args) #phase_shift_from_partial_wave(nucleus,kappa,energy,lepton_mass,**args)
+                
+                if -kappa < N_partial_waves+1:
+                    if lepton_mass==0:
+                        results_dict[-kappa] = results_dict[kappa]
+                    else:
+                        results_dict[-kappa] = pool.apply_async(phase_shift_from_partial_wave, args=(nucleus,-kappa,energy,lepton_mass), kwds=args)
+                        #print('Queued: kappa=',kappa,' , ()',pool_counter+1,'/',N_pools,')')
+                        pool_counter+=1
+                        #phase_shifts[-kappa], phase_differences[-kappa] = phase_shift_from_partial_wave(nucleus,-kappa,energy,lepton_mass,**args) #phase_shift_from_partial_wave(nucleus,kappa,energy,lepton_mass,**args
+                
+                kappa_counter+=1
+            
+                if pool_counter==N_pools:
+                    pool_counter=0
+                    pool.close()
+                    pool.join()
+                    
+                    for key in results_dict:
+                        phase_shift_kappa, phase_difference_kappa =  results_dict[key].get()
+                        phase_shifts[key] = phase_shift_kappa
+                        phase_differences[key] = phase_difference_kappa
+
+                        if np.abs(phase_differences[key])<=phase_difference_limit:
+                            phase_difference_gr0 = False
+                            kappa=-np.abs(key)+1
+                            if verbose:
+                                print("phase differences set to zero after kappa=",kappa)
+                            break
+
+            else:
+                #print(kappa,'0')
+                eta_regular = eta_coulomb(kappa,charge,energy,lepton_mass,reg=+1)
+                phase_shifts[kappa] = delta_coulomb(kappa,charge,energy,lepton_mass,reg=+1,pass_eta=eta_regular) + 0
+                if -kappa < N_partial_waves+1:
+                    if lepton_mass==0:
+                        phase_shifts[-kappa] = phase_shifts[kappa]
+                    else:
+                        phase_shifts[-kappa] = delta_coulomb(-kappa,charge,energy,lepton_mass,reg=+1,pass_eta=eta_regular) + 0
+            
+            kappa-=1
+                        
+
+    nonspinflip = nonspinflip_amplitude(energy,theta,lepton_mass,N_partial_waves,subtractions,phase_shifts)
+    
+    if lepton_mass==0:
+        crosssection = (1+np.tan(theta/2)**2)*np.abs(nonspinflip)**2
+    else:
+        print('Warning: m!=0 does not converge properly, to be revised')
+        #mass_correction = mass_correction_amplitude(energy,theta,lepton_mass,N_partial_waves,phase_shifts)
+        #spinflip = np.tan(theta/2)*nonspinflip + mass_correction
+        spinflip = spinflip_amplitude(energy,theta,lepton_mass,N_partial_waves,subtractions,phase_shifts)
+        crosssection = np.abs(nonspinflip)**2 + np.abs(spinflip)**2
+        
+    return scale_crosssection * crosssection
+
+def crosssection_lepton_nucleus_scattering_singlethreaded(energy,theta,nucleus,lepton_mass=0,recoil=True,subtractions=3,N_partial_waves=100,verbose=False,phase_difference_limit=0,**args):
     
     args['verbose']=verbose
 
@@ -258,6 +366,7 @@ def crosssection_lepton_nucleus_scattering(energy,theta,nucleus,lepton_mass=0,re
         scale_crosssection = 1
     
     phase_shifts = {}
+    phase_differences = {}
     phase_difference_gr0 = True
     # calculate beginning and critical radius only once, since independent on kappa
     if (not ('beginning_radius' in args)) or (not ('critical_radius' in args)):
@@ -269,18 +378,15 @@ def crosssection_lepton_nucleus_scattering(energy,theta,nucleus,lepton_mass=0,re
 
     for kappa in np.arange(-1,-(N_partial_waves+1+1),-1,dtype=int):
         if phase_difference_gr0:    
-            #print(kappa,'calc')
-            partial_wave_kappa = continuumstates(nucleus,kappa,energy,lepton_mass,**args)
-            partial_wave_kappa.extract_phase_shift()
-            phase_shifts[kappa] = partial_wave_kappa.phase_shift
+            
+            phase_shifts[kappa], phase_differences[kappa] = phase_shift_from_partial_wave(nucleus,kappa,energy,lepton_mass,**args) #phase_shift_from_partial_wave(nucleus,kappa,energy,lepton_mass,**args)
+            
             if -kappa < N_partial_waves+1:
                 if lepton_mass==0:
                     phase_shifts[-kappa] = phase_shifts[kappa]
                 else:
-                    partial_wave_mkappa = continuumstates(nucleus,-kappa,energy,lepton_mass,**args)
-                    partial_wave_mkappa.extract_phase_shift()
-                    phase_shifts[-kappa] = partial_wave_mkappa.phase_shift
-                if np.abs(partial_wave_kappa.phase_difference)<=phase_difference_limit:
+                    phase_shifts[kappa], phase_differences[kappa] = phase_shift_from_partial_wave(nucleus,-kappa,energy,lepton_mass,**args) #phase_shift_from_partial_wave(nucleus,kappa,energy,lepton_mass,**args
+                if np.abs(phase_differences[kappa])<=phase_difference_limit:
                     phase_difference_gr0 = False
                     if verbose:
                         print("phase differences set to zero after kappa=",kappa)
