af375e0d50b25fa961c4181157680ec23dd5621e
Partynumbers42 <118724611+Partynumbers42@users.noreply.github.com>
2025-03-18 18:18:09 +0100

    More Asymmetry Crosschecks

--- src/phasr/nuclei/parameterizations/fermi.py
+++ src/phasr/nuclei/parameterizations/fermi.py
@@ -5,7 +5,7 @@ from .numerical import nucleus_num
 import numpy as np
 pi = np.pi
 
-from mpmath import polylog,lerchphi
+from mpmath import lerchphi, fp, polylog
 
 class nucleus_fermi(nucleus_base):
     def __init__(self,name,Z,A,c,z,**args): 
@@ -21,10 +21,12 @@ class nucleus_fermi(nucleus_base):
             self.nucleus_type+="2p"
         self.total_charge=Z
         #
-        self.polylog2 = polylog(2,-np.exp(self.c/self.z))
-        self.polylog3 = polylog(3,-np.exp(self.c/self.z))
-        self.polylog5 = polylog(5,-np.exp(self.c/self.z))
-        self.polylog7 = polylog(7,-np.exp(self.c/self.z))
+        self.polylog2 = float(polylog(2,-np.exp(self.c/self.z)))
+        self.polylog3 = float(polylog(3,-np.exp(self.c/self.z)))
+        self.polylog4 = float(polylog(4,-np.exp(self.c/self.z)))
+        self.polylog5 = float(polylog(5,-np.exp(self.c/self.z)))
+        self.polylog7 = float(polylog(7,-np.exp(self.c/self.z)))
+        self.charge_density_norm = self.total_charge/(4*pi)*1./(-2*self.z**3*self.polylog3-24*self.w*self.z**5*self.polylog5/self.c**2)
         #
         self.nucleus_num = nucleus_num(name+self.nucleus_type,Z=Z,A=A,charge_density=self.charge_density) 
         #
@@ -32,10 +34,10 @@ class nucleus_fermi(nucleus_base):
 
     def update_dependencies(self):
         nucleus_base.update_dependencies(self)
-        self.charge_radius_sq = charge_radius_sq_fermi(self.c,self.z,self.w,self.polylog3,self.polylog5,self.polylog7)
+        self.charge_radius_sq = charge_radius_sq_fermi(self.c,self.z,self.w,self.polylog5,self.polylog7,self.charge_density_norm,self.total_charge)
         self.charge_radius = np.sqrt(self.charge_radius_sq) if self.charge_radius_sq>=0 else np.sqrt(self.charge_radius_sq+0j)
-        #electric_potential_V0_fermi(self.z,self.polylog2,self.polylog3,self.total_charge)
-        
+        self.Vmin_ana = electric_potential_V0_fermi(self.c,self.z,self.w,self.polylog2,self.polylog4,self.charge_density_norm)
+    
     def set_form_factor_from_charge_density(self):
         self.nucleus_num.set_form_factor_from_charge_density()
         self.form_factor = self.nucleus_num.form_factor
@@ -49,65 +51,66 @@ class nucleus_fermi(nucleus_base):
         self.update_dependencies()
 
     def charge_density(self,r):
-        return charge_density_fermi(r,self.c,self.z,self.w,self.polylog3,self.polylog5,self.total_charge)
+        return charge_density_fermi(r,self.c,self.z,self.w,self.charge_density_norm)
     
     def electric_field_ana(self,r):
-        print('warning: analytical field not as precise')
-        return electric_field_fermi(r,self.c,self.z,self.w,self.polylog3,self.polylog5,self.total_charge)
+        print('warning: analytical field not precise enough')
+        return electric_field_fermi(r,self.c,self.z,self.w,self.polylog3,self.polylog5,self.charge_density_norm)
     
     def electric_potential_ana(self,r):
-        # wrong for some reason 
-        print('warning: analytical potential is wrong')
-        return electric_potential_fermi(r,self.c,self.z,self.w,self.polylog2,self.polylog3,self.polylog5,self.total_charge)
+        print('warning: analytical potential not precise enough')
+        return electric_potential_fermi(r,self.c,self.z,self.w,self.polylog2,self.polylog3,self.polylog4,self.polylog5,self.charge_density_norm)
     
-    # too slow (-> find better implementation/representation)
-    def form_factor_ana(self,r):
-         return form_factor_fermi(r,self.c,self.z,self.w)
+    # def form_factor_ana(self,r):
+    #     print('warning: analytical form factor very slow and wrong')
+    #     return form_factor_fermi(r,self.c,self.z,self.w,self.charge_density_norm,self.total_charge)
 
-def charge_density_fermi(r,c,z,w,polylog3,polylog5,Z):
-    rho0 = Z/(4*pi*float(-2*z**3*polylog3-24*w*z**5*polylog5/c**2))
-    return rho0*(1+ w*r**2/c**2)*np.exp(-(r-c)/z)/(1+np.exp(-(r-c)/z))
+def charge_density_fermi(r,c,z,w,norm):
+    return norm*(1+ w*r**2/c**2)*np.exp(-(r-c)/z)/(1+np.exp(-(r-c)/z))
 
-def charge_radius_sq_fermi(c,z,w,polylog3,polylog5,polylog7):
-    return float(12*(c**2*z**2*polylog5+30*w*z**4*polylog7)/(c**2*polylog3+12*w*z**2*polylog5))
+def charge_radius_sq_fermi(c,z,w,polylog5,polylog7,norm,Z):
+    #
+    poly5=polylog5
+    poly7=polylog7
+    return -4*pi*norm/Z*24*(poly5*z**5 + (30*poly7*w*z**7)/c**2)
 
-# do not use these:
+def electric_potential_V0_fermi(c,z,w,polylog2,polylog4,norm,alpha_el=constants.alpha_el):
+    # not as precise as numerically
+    poly2=polylog2
+    poly4=polylog4
+    return 4*pi*alpha_el*norm*z**2*(poly2 + 6*poly4*w*z**2/c**2)
 
-def electric_potential_V0_fermi(z,polylog2,polylog3,Z,alpha_el=constants.alpha_el):
-    # not correct
-    return float(-Z*alpha_el*polylog2/(2*z*polylog3))
-
-def electric_field_fermi_scalar(r,c,z,w,polylog3,polylog5,Z,alpha_el=constants.alpha_el):
-    # something wrong with this and slow (faster with fp, but still wrong)
+def electric_field_fermi_scalar(r,c,z,w,polylog3,polylog5,norm,alpha_el=constants.alpha_el):
+    # slow and not as precise as numerically
     poly1r = np.log(1 + np.exp((c - r)/z))
-    poly2r = polylog(2,-np.exp((c - r)/z))
-    poly3r = polylog(3,-np.exp((c - r)/z))
-    poly4r = polylog(4,-np.exp((c - r)/z))
-    poly5r = polylog(5,-np.exp((c - r)/z))
+    poly2r = fp.polylog(2,-np.exp((c - r)/z))
+    poly3r = fp.polylog(3,-np.exp((c - r)/z))
+    poly4r = fp.polylog(4,-np.exp((c - r)/z))
+    poly5r = fp.polylog(5,-np.exp((c - r)/z))
     poly3 = polylog3
     poly5 = polylog5 
-    return Z*np.sqrt(alpha_el)/np.sqrt(4*pi)*float(poly1r*r**2*(c**2 + r**2*w) - 2*poly2r*r*(c**2 + 2*r**2*w)*z + 2*z**2*(c**2*poly3 - poly3r*(c**2 + 6*r**2*w) - 12*w*z*(poly4r*r - poly5*z + poly5r*z)))/float(2.*r**2*z**2*(c**2*poly3 + 12*poly5*w*z**2))
+    return  np.sqrt(4*pi*alpha_el)*norm*(z*(-(poly1r*r**2*(c**2 + r**2*w)) + 2*z*(poly2r*r*(c**2 + 2*r**2*w) +  z*(-(c**2*poly3) + poly3r*(c**2 + 6*r**2*w) + 12*w*z*(poly4r*r - poly5*z + poly5r*z)))))/(c**2*r**2)
 electric_field_fermi = np.vectorize(electric_field_fermi_scalar,excluded=[1,2,3,4,5,6,7])
 
-def electric_potential_fermi_scalar(r,c,z,w,polylog2,polylog3,polylog5,Z,alpha_el=constants.alpha_el): 
-    # something very wrong with this and slow (faster with fp, but still wrong)
-    poly1r = np.log(1 + np.exp((c - r)/z))
-    poly2r = polylog(2,-np.exp((c - r)/z))
-    poly3r = polylog(3,-np.exp((c - r)/z))
-    poly4r = polylog(4,-np.exp((c - r)/z))
-    poly5r = polylog(5,-np.exp((c - r)/z))
+def electric_potential_fermi_scalar(r,c,z,w,polylog2,polylog3,polylog4,polylog5,norm,alpha_el=constants.alpha_el): 
+    # slow and not as precise as numerically
+    poly2r = fp.polylog(2,-np.exp((c - r)/z))
+    poly3r = fp.polylog(3,-np.exp((c - r)/z))
+    poly4r = fp.polylog(4,-np.exp((c - r)/z))
+    poly5r = fp.polylog(5,-np.exp((c - r)/z))
     poly3 = polylog3
     poly5 = polylog5
     if r==0:
-        return electric_potential_V0_fermi(z,polylog2,polylog3,Z,alpha_el)
+        return electric_potential_V0_fermi(c,z,w,polylog2,polylog4,norm,alpha_el)
     else:
-        return Z*alpha_el*float(-2*c**2*poly3**2*z**2 + 12*poly5*r*w*z**2*(poly1r*r - poly2r*z) + poly3*(-(poly1r*r**4*w) + poly2r*r*(c**2 + 4*r**2*w)*z + 2*poly3r*(c**2 + 6*r**2*w)*z**2 + 24*w*z**3*(poly4r*r + (-poly5 + poly5r)*z)))/float(2.*poly3*r*z**2*(c**2*poly3 + 12*poly5*w*z**2))     
-electric_potential_fermi = np.vectorize(electric_potential_fermi_scalar,excluded=[1,2,3,4,5,6,7,8])
+        return -4*pi*alpha_el*norm*z**2*(poly2r*r*(c**2 + r**2*w) + 2*z*(-(c**2*poly3) + poly3r*(c**2 + 3*r**2*w) + 3*w*z*(3*poly4r*r - 4*poly5*z + 4*poly5r*z)))/(c**2*r)
+electric_potential_fermi = np.vectorize(electric_potential_fermi_scalar,excluded=[1,2,3,4,5,6,7,8,9])
 
-def form_factor_fermi_scalar(q,c,z,w):
-    # way to slow (lerchphi), unclear if correct
-    q=q/constants.hc
-    lp2 = np.real(1j*( lerchphi(-np.exp(c/z),2,1-1j*q*z) - lerchphi(-np.exp(c/z),2,1+1j*q*z) ))
-    lp4 = np.real(1j*( lerchphi(-np.exp(c/z),4,1-1j*q*z) - lerchphi(-np.exp(c/z),4,1+1j*q*z) ))
-    return float(np.exp(c/z)*(c**2*lp2 + 6*w*z**2*lp4)/(4*q*z*(c**2*polylog(3,-np.exp(c/z))+12*w*z**2*polylog(5,-np.exp(c/z)))))
-form_factor_fermi = np.vectorize(form_factor_fermi_scalar,excluded=[1,2,3])
+# def form_factor_fermi_scalar(q,c,z,w,norm,Z):
+#     # way to slow (lerchphi)
+#     # wrong (b/c wrong sheet???)
+#     q=q/constants.hc
+#     lp2 = 1j*( lerchphi(-np.exp(c/z),2,1-1j*q*z) - lerchphi(-np.exp(c/z),2,1+1j*q*z) )
+#     lp4 = 1j*( lerchphi(-np.exp(c/z),4,1-1j*q*z) - lerchphi(-np.exp(c/z),4,1+1j*q*z) )
+#     return - 4*pi*norm/Z*np.exp(c/z)*z**2*float(np.real(c**2*lp2 + 6*w*z**2*lp4))/(2*c**2*q)
+# form_factor_fermi = np.vectorize(form_factor_fermi_scalar,excluded=[1,2,3,4,5])
