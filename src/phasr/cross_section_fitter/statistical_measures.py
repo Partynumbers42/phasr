caee2dfd46b2711cf3c557cd7e2d223dec9acde0
Partynumbers42 <118724611+Partynumbers42@users.noreply.github.com>
2025-07-16 17:56:30 +0200

    Update statistical_measures.py

--- src/phasr/cross_section_fitter/statistical_measures.py
+++ src/phasr/cross_section_fitter/statistical_measures.py
@@ -8,7 +8,8 @@ class minimization_measures():
     def __init__(self,test_function,x_data,y_data,cov_stat_data,cov_syst_data):
         ''' 
         test_function of type fct(x,*param,**params) 
-        x_data, y_data, dy_data are 1d arrays of the same length
+        x_data and y_data are 1d arrays of the same length 
+        cov_stat_data and cov_syst_data are corresponding 2d arrays 
         '''
         
         self.test_function = test_function
@@ -21,8 +22,13 @@ class minimization_measures():
         self.cov_stat_data = cov_stat_data
         self.cov_syst_data = cov_syst_data
         
+        self.dy_data = np.sqrt(cov_stat_data.diagonal() + cov_syst_data.diagonal())
+        
         self.set_cov()
-    
+        
+        # check for non-zero off diagonal elements
+        self.consider_dAgostini_bias = np.sum(np.abs(self.cov_data)) > np.sum(np.abs(self.dy_data**2))
+        
     def set_cov(self,*params_args,**params_kwds):
         y_test = self.test_function(self.x_data,*params_args,**params_kwds)       
         scale_syst = y_test/self.y_data
@@ -30,41 +36,92 @@ class minimization_measures():
         self.cov_data = self.cov_stat_data + cov_syst_data_rescaled
         self.inv_cov_data = inv(self.cov_data)
     
-    def residual(self,*params_args,**params_kwds):
+    def residual(self,*params_args,weighted=True,**params_kwds):
         y_test = self.test_function(self.x_data,*params_args,**params_kwds)       
-        return (y_test - self.y_data)/self.dy_data
-    
+        return (y_test - self.y_data)/(self.dy_data if weighted else 1. ) 
+        
     def loss(self,*params_args,**params_kwds):
-        residual = self.residual(*params_args,**params_kwds)
+        residual = self.residual(*params_args,weighted=False,**params_kwds)
         return np.einsum('i,ij,j',residual,self.inv_cov_data,residual)
 
+def ai_abs_bounds_default(ni,R,Z):
+    return (1./ni)*(Z*pi**2)/(2*R**3)
 
 class Parameter_set():
-    def __init__(self,R:float,ai=None,xi=None):
+    
+    def __init__(self,R:float,total_charge:float,ai=None,xi=None,ai_abs_bound=None):
         
-        self.R = R
-        self.xi = xi 
-        self.ai = ai
-        self.alim = (1./nu)*(Z*pi**2)/(2*R**3)
-    
-    
-    def update_dependencies():
-        pass
+        self.R = R 
+        self.total_charge = total_charge
         
+        if xi is not None:
+            self.xi = xi 
+            self.N_x = len(self.xi)
+            self.N_a = self.N_x + 1
+        elif ai is not None:
+            self.ai = ai
+            self.N_a = len(self.ai)
+            self.N_x = self.N_a - 1
+        else:
+            raise ValueError("Need to supply either ai or xi!") 
+        
+        self.ni = np.arange(1,self.N_a+1)
+        self.qi=np.arange(1,self.N_a+1)*pi/self.R
+        
+        if ai_abs_bound is None:
+            self.ai_abs_bound = ai_abs_bounds_default(self.ni,self.R,total_charge)
+        else:
+            self.ai_abs_bound = ai_abs_bound
+        
+        if not hasattr(self,"xi"):
+            self.update_ai_then_xi(self.ai)
+        
+        if not hasattr(self,"ai"):
+            self.update_xi_then_ai(self.xi)
+        
+    def update_ai_then_xi(self,ai):
+        self.ai=ai
+        self.set_ai_tilde_from_ai()
+        self.set_xi_from_ai_tilde()
     
+    def update_xi_then_ai(self,xi):
+        self.xi=xi
+        self.set_ai_tilde_from_xi()
+        self.set_ai_from_ai_tilde()
+        
+    def set_xi_from_ai_tilde(self):
+        xi_dummy = np.zeros(self.N_x)
+        for j in range(1,self.N_x+1):
+            aj_tilde_implied_min, aj_tilde_implied_max = ai_tilde_implied_bounds(j,self.ai_tilde,self.ai_abs_bound,self.qi,self.N_a,self.R,self.total_charge)
+            xi_dummy[j-1] = (self.ai_tilde[j-1] - aj_tilde_implied_min[:-1])/(aj_tilde_implied_max[:-1]-aj_tilde_implied_min[:-1])        
+        self.xi = xi_dummy
+    
+    def set_ai_tilde_from_xi(self):
+        ai_tilde_dummy = np.zeros(self.N_a)
+        for j in range(1,self.N_a+1):
+            aj_tilde_implied_min, aj_tilde_implied_max = ai_tilde_implied_bounds(j,self.ai_tilde,self.ai_abs_bound,self.qi,self.N_a,self.R,self.total_charge)
+            ai_tilde_dummy[j-1] = self.xi[j-1]*(aj_tilde_implied_max-aj_tilde_implied_min) + aj_tilde_implied_min if j<self.N_a else aj_tilde_implied_min
+        self.ai_tilde = ai_tilde_dummy
+    
+    def set_ai_tilde_from_ai(self):
+        self.ai_tilde = self.ai*(-1)**(self.ni+1)
+    
+    def set_ai_from_ai_tilde(self):
+        self.ai = self.ai_tilde*(-1)**(self.ni+1)
+    
+    def get_ai(self):
+        return self.ai
+    
+    def get_xi(self):
+        return self.xi
 
+def ai_tilde_implied_bounds(j,ai_tilde,ai_abs_bound,qi,N_a,R,total_charge):
+    
+    qj = qi[j-1]
+    mean = total_charge/(4*pi*R) - np.sum(ai_tilde[0:j-1]/qi[0:j-1]**2)
+    spread = np.sum(ai_abs_bound[j:N_a+1]/qi[j:N_a+1]**2)
 
-def a_lim(nu,Z,R):
-    return (1./nu)*(Z*pi**2)/(2*R**3)
+    ai_tilde_implied_max=min(qj**2*(mean + spread),ai_abs_bound[j-1])
+    ai_tilde_implied_min=max(qj**2*(mean - spread),-ai_abs_bound[j-1])
 
-def xi_ai(ai,ai_tilde_lim,Z,R):
-
-    N=len(ai_tilde_lim)
-    ai_t=ai_tilde(ai)
-    nus=np.arange(1,N+1)
-
-    ai_tilde_min, ai_tilde_max = ai_impl_border_vec(nus,ai_t,ai_tilde_lim,Z,R)
-
-    xi=(ai_t[:-1] - ai_tilde_min[:-1])/(ai_tilde_max[:-1]-ai_tilde_min[:-1])
-
-    return xi
+    return  ai_tilde_implied_min, ai_tilde_implied_max
